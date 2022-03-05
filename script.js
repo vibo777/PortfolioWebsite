8279549e17a3c9f41b5d21b956373b93637736fb
vibo777 <54715728+vibo777@users.noreply.github.com>
2022-03-05 22:04:59 +0530

    update

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger =  document.querySelector(".hamburger");  
-const headermenu = document.querySelector(".header-menu");
-
-  hamburger.addEventListener("click",() => {
-  hamburger.classList.toggle("active");
-  headermenu.classList.toggle("active"); 
-})
